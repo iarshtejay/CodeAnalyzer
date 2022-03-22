e99b7893c87fc2548a5a41e8f04686245bd552cc
Bharatwaaj Shankar <bharatwaaj.1996@gmail.com>
2022-03-22 20:42:48 -0300

    fixing mc

--- codeanalyzer-frontend/src/lib/api/index.js
+++ codeanalyzer-frontend/src/lib/api/index.js
@@ -9,6 +9,19 @@ const createAuths = (info, headers) => {
     return post('/auths', info, headers);
 }
 
+// const fetchGithubRepo = async (username)=>{
+//     const url = "https://api.github.com/users/tushartushar/repos";
+
+//     const response = await get(url)
+//     //console.log(response.data)
+//     cleanData(response.data)
+// }
+
+const lengthOfFetchedData = async (url) => {
+  return await get(url).length;
+};
+
+
 export const api = {
     authGithubUser,
     createAuths
