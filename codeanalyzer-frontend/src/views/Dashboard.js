5a3d11095c087671c28b82b5b1787515126ef6a2
Arshdeep Singh <ar899345@dal.ca>
2022-04-05 20:08:39 -0300

    Add a for loop to map to table

--- codeanalyzer-frontend/src/views/Dashboard.js
+++ codeanalyzer-frontend/src/views/Dashboard.js
@@ -319,46 +319,12 @@ const Dashboard = (props) => {
                   </tr>
                 </thead>
                 <tbody>
-                    {}
-                  <tr>
-                    <th scope="row">/argon/</th>
-                    <td>4,569</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">/argon/index.html</th>
-                    <td>3,985</td>
-                    <td>319</td>
-                    <td>
-                      <i className="fas fa-arrow-down text-warning mr-3" />{" "}
-                      46,53%
-                    </td>
-                  </tr>
-                  <tr>
-                    <th scope="row">/argon/charts.html</th>
-                    <td>3,513</td>
-                    <td>294</td>
-                    <td>
-                      <i className="fas fa-arrow-down text-warning mr-3" />{" "}
-                      36,49%
-                    </td>
-                  </tr>
-                  <tr>
-                    <th scope="row">/argon/tables.html</th>
-                    <td>2,050</td>
-                    <td>147</td>
-                    <td>
-                      <i className="fas fa-arrow-up text-success mr-3" /> 50,87%
-                    </td>
-                  </tr>
-                  <tr>
-                    <th scope="row">/argon/profile.html</th>
-                    <td>1,795</td>
-                    <td>190</td>
-                    <td>
-                      <i className="fas fa-arrow-down text-danger mr-3" />{" "}
-                      46,53%
-                    </td>
-                  </tr>
+                    {Object.keys(smellData).map((keyName, i) => {
+                        <tr>
+                            <th scope="row">{keyName}</th>
+                            <td>{smellData[keyName]}</td>
+                        </tr>})
+                    }
                 </tbody>
               </Table>
             </Card>
