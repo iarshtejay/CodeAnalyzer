b7b5a40282b9dfefaa265c66f69e64a1f1334316
Arshdeep Singh <ar899345@dal.ca>
2022-04-05 23:02:36 -0300

    add row logic

--- codeanalyzer-frontend/src/views/Dashboard.js
+++ codeanalyzer-frontend/src/views/Dashboard.js
@@ -329,50 +329,18 @@ const Dashboard = (props) => {
                                     <th scope="col">Count</th>
                                 </tr>
                                 </thead>
-                                <tbody>
-                                    <tr>
-                                        <th scope="row">/argon/</th>
-                                        <td>4,569</td>
-                                        <td>340</td>
-                                        <td>
-                                            <i className="fas fa-arrow-up text-success mr-3" /> 46,53%
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">/argon/index.html</th>
-                                        <td>3,985</td>
-                                        <td>319</td>
-                                        <td>
-                                            <i className="fas fa-arrow-down text-warning mr-3" />{" "}
-                                            46,53%
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">/argon/charts.html</th>
-                                        <td>3,513</td>
-                                        <td>294</td>
-                                        <td>
-                                            <i className="fas fa-arrow-down text-warning mr-3" />{" "}
-                                            36,49%
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">/argon/tables.html</th>
-                                        <td>2,050</td>
-                                        <td>147</td>
-                                        <td>
-                                            <i className="fas fa-arrow-up text-success mr-3" /> 50,87%
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">/argon/profile.html</th>
-                                        <td>1,795</td>
-                                        <td>190</td>
-                                        <td>
-                                            <i className="fas fa-arrow-down text-danger mr-3" />{" "}
-                                            46,53%
-                                        </td>
-                                    </tr>
+                                    <tbody>
+                                    {
+                                        loadedSmells ? Object.keys(smells).map((keyName,i)=>{
+                                            return(
+                                                <tr>
+                                                <th scope="row">{keyName}</th>
+                                                <td>{smells[keyName]}</td>
+                                        </tr>
+                                            )
+                                        }) :
+                                        <tr>   Fetching...</tr>
+                                    }
                                 </tbody>
                             </Table>
                         </Card>
