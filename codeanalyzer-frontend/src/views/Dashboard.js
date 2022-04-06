d722964e86e25ef223c1e72c05c648e6fe556c88
Arshdeep Singh <ar899345@dal.ca>
2022-04-05 22:59:54 -0300

    modify template table on dashboard

--- codeanalyzer-frontend/src/views/Dashboard.js
+++ codeanalyzer-frontend/src/views/Dashboard.js
@@ -307,7 +307,7 @@ const Dashboard = (props) => {
                             <CardHeader className="border-0">
                                 <Row className="align-items-center">
                                     <div className="col">
-                                        <h3 className="mb-0">Page visits</h3>
+                                        <h3 className="mb-0">Smells Summary</h3>
                                     </div>
                                     <div className="col text-right">
                                         <Button
